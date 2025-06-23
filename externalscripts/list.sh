84ec04f4c55c5716178f81ad083d541b0b91ae24
Nils Israel <israel@agro.eu>
2025-06-23 11:48:43 +0200

    only allow the fixed config filename as paramter

--- externalscripts/list.sh
+++ externalscripts/list.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-cd /usr/lib/zabbix/externalscripts/
-cat $1
+cd /usr/lib/zabbix/externalscripts/ || exit 1
+
+if [[ "$1" != "ssl_check.json" ]]; then
+  echo "Error: Only 'ssl_check.json' is allowed as a parameter."
+  exit 1
+fi
+
+cat "$1"
